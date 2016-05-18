c3ee30fd82a6477a26af3c56c4df028017aa3626
Nishant Dubey <nishant05dubey@gmail.com>
2016-05-18 16:44:27 +0530

    Modify spalsh UI

--- app/src/main/java/com/bignerdranch/android/splash/App.java
+++ app/src/main/java/com/bignerdranch/android/splash/App.java
@@ -13,5 +13,6 @@ public class App extends Application {
 
         // Don't do this! This is just so cold launches take some time
         SystemClock.sleep(TimeUnit.SECONDS.toMillis(3));
+
     }
 }
